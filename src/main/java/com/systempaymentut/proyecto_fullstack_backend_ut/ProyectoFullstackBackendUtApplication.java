ed121d1e3146d4dbd561fd144fd53637ecdc51c3
santuzzxooy <santiagorochav@gmail.com>
2025-03-24 22:23:34 -0500

    initial comit
    vamos que vamosss

---
+++ src/main/java/com/systempaymentut/proyecto_fullstack_backend_ut/ProyectoFullstackBackendUtApplication.java
@@ -0,0 +1,13 @@
+package com.systempaymentut.proyecto_fullstack_backend_ut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFullstackBackendUtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFullstackBackendUtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systempaymentut/proyecto_fullstack_backend_ut/ProyectoFullstackBackendUtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systempaymentut.proyecto_fullstack_backend_ut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFullstackBackendUtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
